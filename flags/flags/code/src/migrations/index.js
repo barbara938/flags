f1b30731a1f482105b2d14526c48fe6d0378b677
barbara938 <61320904+barbara938@users.noreply.github.com>
2020-10-13 14:30:29 -0300

    Initial commit

---
+++ flags/flags/code/src/migrations/index.js
@@ -0,0 +1,39 @@
+const { conn } = require("../db");
+
+async function up() {
+  const { conn } = require("../db");
+
+  async function up() {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS flags (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      image TEXT
+    )
+  `;
+
+    const db = await conn();
+
+    await db.run(sql);
+  }
+
+  async function down() {
+    const sql = `DROP TABLE flags`;
+
+    const db = await conn();
+
+    await db.run(sql);
+  }
+
+  module.exports = { up, down };
+}
+
+async function down() {
+  const sql = `DROP TABLE flags`;
+
+  const db = await conn();
+
+  await db.run(sql);
+}
+
+module.exports = { up, down };
